36bdb7e6f2c512f7e8956605049bcb5e0e9b34b4
JAY-PURPLEBRICK\Jay <jay.espiritu@purplebricks.com.au>
2018-04-10 13:27:10 +1000

    Merge action methods

--- src/main/java/Base/BaseActions.java
+++ src/main/java/Base/BaseActions.java
@@ -1,12 +1,23 @@
 package Base;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
 
 public class BaseActions {
 
@@ -86,6 +97,27 @@ public class BaseActions {
 
         }
     }
+
+    public void uploadFile(By UploadLocator, By SubmitLocator, String fileName, String filePath) {
+        File file = new File(fileName);
+        filePath = file.getAbsolutePath();
+        type(filePath, UploadLocator);
+        click(SubmitLocator);
+    }
+
+    public void downloadFile() throws IOException {
+        String link = driver.findElement(By.cssSelector(".example a:nth-of-type(1)")).
+                getAttribute("href");
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpHead request = new HttpHead(link);
+        HttpResponse response = httpClient.execute(request);
+        String contentType = response.getFirstHeader("Content-Type").getValue();
+        int contentLength = Integer.parseInt(response.getFirstHeader("Content-Length").getValue
+                ());
+        assertThat(contentType, is("application/octet-stream"));
+        assertThat(contentLength, is(not(0)));
+    }
+
     /**
      * Assertion methods
      */
